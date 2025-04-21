e5623924c037e8a36d2d12c352ad2b4bcd86b639
Jenny Ibanez cano <jenny.ibanezcano@gmail.com>
2025-04-21 17:40:55 +0100

    Adding the DAO file and index html

---
+++ boardgamesDAO.py
@@ -0,0 +1,100 @@
+# create  boardgamesDAO as a database access object (DAO) for the boardgames table
+# Author: Jenny Ibanez Cano
+
+
+import mysql.connector
+import dbconfig as db 
+
+class BoardgamesDAO:
+    connection=""
+    cursor=""
+    host=""
+    user=""
+    password=""
+    database=""
+    
+    def __init__(self):
+        self.host = db.mysql['host']
+        self.user = db.mysql['user']
+        self.password = db.mysql['password']
+        self.database = db.mysql['database']
+        
+    def getcursor(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.connection.cursor()
+        return self.cursor
+    
+    def closeAll(self):
+        self.connection.close()
+        self.cursor.close()
+         
+    def getAll(self):
+        cursor = self.getcursor()
+        sql="select * from boardgames"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        #print(results)
+        for result in results:
+            #print(result)
+            returnArray.append(self.convertToDictionary(result))
+        
+        self.closeAll()
+        return returnArray
+
+    def findByID(self, id):
+        cursor = self.getcursor()
+        sql="select * from boardgames where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        returnvalue = self.convertToDictionary(result)
+        self.closeAll()
+        return returnvalue
+
+    def create(self, boardgame):
+        cursor = self.getcursor()
+        sql="insert into boardgames (Name,Product type, Age range, Players, Price) values (%s,%s,%s,%s,%s)"
+        values = (boardgame.get("Name"), boardgame.get("Product type"), boardgame.get("Age Range"), boardgame.get("Players"), boardgame.get("Price"))
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        newid = cursor.lastrowid
+        boardgame["id"] = newid
+        self.closeAll()
+        return boardgame
+    
+    def update(self, id, boardgame):
+        cursor = self.getcursor()
+        sql="update boardgames set Name= %s, Product type=%s, Age range=%s, Players=%s, Price=%s where id = %s"
+        
+        values = (boardgame.get("Name"), boardgame.get("Product type"), boardgame.get("Age Range"), boardgame.get("Players"), boardgame.get("Price"), id)
+        cursor.execute(sql, values)
+        self.connection.commit()
+        self.closeAll()
+        
+    def delete(self, id):
+        cursor = self.getcursor()
+        sql="delete from book where id = %s"
+        values = (id,)
+        cursor.execute(sql, values)
+        self.connection.commit()
+        self.closeAll()
+        print("delete done")
+
+    def convertToDictionary(self, resultLine):
+        attkeys=['id','Name','Product type', 'Age range', 'Players', 'Price']
+        boardgame = {}
+        currentkey = 0
+        for attrib in resultLine:
+            boardgame[attkeys[currentkey]] = attrib
+            currentkey = currentkey + 1 
+        return boardgame
+
+boardgamesDAO = BoardgamesDAO()
